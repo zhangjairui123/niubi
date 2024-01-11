d30efa15ea85ba92ea14f232425bb7e16449fed7
Your Name <email@example.com>
2024-01-11 15:36:57 +0800

    第二次修改

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("嘎嘎牛逼!---非常牛逼");
+        System.out.println("牛");
     }
 }
