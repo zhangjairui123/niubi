9f3291994e153c852963016b28f4ace673651fad
Your Name <email@example.com>
2024-01-11 15:28:03 +0800

    第一次修改

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("嘎嘎牛逼!");
+        System.out.println("嘎嘎牛逼!---非常牛逼");
     }
 }
